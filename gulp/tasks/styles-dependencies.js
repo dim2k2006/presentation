6be43283e033bbdb229fd9016b1e2e4e113e7298
Дмитрий Малофеев <dim2k2006@yandex.ru>
2017-01-14 14:52:52 +0300

    Fix fonts

--- gulp/tasks/styles-dependencies.js
+++ gulp/tasks/styles-dependencies.js
@@ -1,6 +1,7 @@
 import gulp       from 'gulp';
 import minifyCss  from 'gulp-minify-css';
 import concatCss  from 'gulp-concat-css';
+import replace    from 'gulp-replace';
 import settings   from '../settings';
 import vendor     from '../../package.json';
 
@@ -8,5 +9,6 @@ gulp.task('styles-dependencies', () => {
     gulp.src(vendor.vendorStyles.path, {base: './src/bower_components/'})
         .pipe(concatCss('vendor.min.css'))
         .pipe(minifyCss())
+        .pipe(replace('reveal.js/lib/font/source-sans-pro/', '/fonts/source-sans-pro/'))
         .pipe(gulp.dest(settings.dist.styles));
 });
